88b00f94acc4e0ea11cb59c490f70b77586d2dc0
Prisoner1992 <abiyevseymur92@gmail.com>
2017-05-10 12:22:29 +0400

    Ardici reqemler

---
+++ ardıcılOlanRəqəmlər/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ardıcılOlanRəqəmlər
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 500;
+            int min = 123;
+            int differ = 111;
+            int result = 0;
+            for (int i = 0; i < b; i += differ)
+            {
+                result = min + i;
+                if (result > b)
+                {
+                    break;
+                }
+                Console.WriteLine(result);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
